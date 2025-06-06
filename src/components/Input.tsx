2980eb6aa054d290ee8b5e64feb2a3cc1a716d1b
Vitor Stosick <vitor.stosick@alunos.sc.senac.br>
2025-06-06 11:07:30 -0300

    component: add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,70 @@
+import { FormControl, InputField, Input as GluestackInput, Text, FormControlError, FormControlErrorText } from "@gluestack-ui/themed";
+import { Icon } from "phosphor-react-native";
+import { ComponentProps } from "react";
+import { gluestackUIConfig } from "../../config/gluestack-ui.config";
+
+
+type Props = ComponentProps<typeof InputField> & {
+  label?: string;
+  icon?: Icon;
+  errorMessage?: string | null;
+  isInvalid?: boolean;
+  isReadOnly?: boolean;
+};
+
+export function Input({
+  label,
+  icon: IconComponent,
+  isReadOnly,
+  errorMessage = null,
+  isInvalid = false,
+  ...rest
+}: Props) {
+  const theme = gluestackUIConfig.tokens.colors;
+
+  const invalid = !!errorMessage || isInvalid;
+
+  return (
+    <FormControl isInvalid={invalid} w="$full">
+      {label && (
+        <Text size="md" fontWeight="$bold" marginBottom="$2" color="$black">
+          {label}
+        </Text>
+      )}
+      <GluestackInput
+        isInvalid={invalid}
+        h="$12"
+        px={IconComponent ? "$4" : "$2"}
+        gap="$1"
+        bg="$base600"
+        borderWidth="$1"
+        borderColor="$base500"
+        rounded="$lg"
+        alignItems="center"
+        $focus={{
+          borderWidth: 1,
+          borderColor: invalid ? "$red500" : "$base400",
+        }}
+        $invalid={{
+          borderWidth: 1,
+          borderColor: "$red500",
+        }}
+        isReadOnly={isReadOnly}
+        opacity={isReadOnly ? 0.5 : 1}
+      >
+        <InputField
+          color="$black"
+          fontFamily="$body"
+          placeholderTextColor="$base400"
+          {...rest}
+        />
+      </GluestackInput>
+
+      <FormControlError>
+        <FormControlErrorText color="$red500">
+          {errorMessage}
+        </FormControlErrorText>
+      </FormControlError>
+    </FormControl>
+  );
+}
--- src/screens/Expenses.tsx
+++ src/screens/Expenses.tsx
@@ -1,13 +1,33 @@
-import { Box, Center, Text } from "@gluestack-ui/themed";
+import { Input } from "@/components/CustomTextInput";
+import { Box, Center, Text, VStack } from "@gluestack-ui/themed";
+import { useState } from "react";
 
 export function Expenses() {
+
+  const [value, setValue] = useState('');
+  const [month, setMonth] = useState('');
+
   return (
-    <Center flex={1}>
-      <Box p="$4" bg="$green">
-        <Text fontWeight="$bold" fontSize="$3xl" color="$white">
-          Expenses
-        </Text>
-      </Box>
+    <Center flex={1} px='$12'>
+        <Box p="$4">
+          <Text fontWeight="$bold" fontSize="$3xl" color="$black">
+            Despesa
+          </Text>
+        </Box>
+        <Box w='$full'>
+          <Input
+            label="Valor"
+            placeholder="Valor"
+            value={value}
+            onChangeText={setValue}
+          />
+          <Input
+            label="Mês"
+            placeholder="Mês"
+            value={month}
+            onChangeText={setMonth}
+          />
+        </Box>
     </Center>
   );
 }
