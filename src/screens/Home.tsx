5799b73897e1fe0c2fe44a2796f9f991d69ca950
matheusbarcc <matheusbarcon@gmail.com>
2025-06-26 15:35:11 -0300

    feat: add api implementation to the profile screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,16 +3,12 @@ import { useAuth } from "@/hooks/useAuth";
 import { Box, Center, Text } from "@gluestack-ui/themed";
 
 export function Home() {
-  const { signOut } = useAuth();
-
   return (
     <Center flex={1}>
       <Box p="$4" bg="$green">
         <Text fontWeight="$bold" fontSize="$3xl" color="$white">
           Home
         </Text>
-
-        <Button label="signout" onPress={signOut} />
       </Box>
     </Center>
   );
